45c41e092046c063901f54e6813572e451155e04
leon-js <42924541+leon-js@users.noreply.github.com>
2025-04-27 15:55:44 +0800

    feat(nocobase):补充类型映射

--- src/nocobase/utils.ts
+++ src/nocobase/utils.ts
@@ -27,13 +27,28 @@ const TYPE_MAP: Record<string, string> = {
   hasMany: "array",
   array: "array",
   dateOnly: "string",
+  integer: "number",
+  decimal: "number",
+  text: "string",
+  boolean: "boolean",
+  float: "number",
+  date: "string",
+  double: "number",
+  /** 自动编码 */
+  sequence: "string",
+  /** [TODO] 根据 公式类型的 Storage type 做判断 */
+  formula: "string",
+  /** 点，基于地图的定位 [number, number] */
+  point: "array",
+  uid: "string",
+  password: "string",
 };
 
 const fieldConvert = (field: Fields[0]) => {
   if (field.uiSchema) {
     const { uiSchema } = field;
     uiSchema.title = getLocaleText(uiSchema.title) || field.name;
-    uiSchema.type = uiSchema.type || TYPE_MAP[field.type];
+    uiSchema.type = TYPE_MAP[field.type];
 
     if (!uiSchema["x-read-pretty"]) {
       // 非只读的，转换x-component
@@ -45,7 +60,7 @@ const fieldConvert = (field: Fields[0]) => {
         X_COMPONENT_MAP[uiSchema["x-component"]] || uiSchema["x-component"];
     }
 
-    if (!uiSchema.type) {
+    if (!TYPE_MAP[field.type]) {
       console.warn(`[nocobase type] 待处理`, field);
     }
   }
